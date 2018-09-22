ff7c9cab6f8914b70af8f69e5550f14dd8db019a
Nut <59070140@kmitl.ac.th>
2018-09-22 21:00:46 +0700

    Initial commit

---
+++ app/src/main/java/com/poowanut59070140/healthy/MainActivity.java
@@ -0,0 +1,19 @@
+package com.poowanut59070140.healthy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_view, new LoginFragment())
+                    .commit();
+        }
+    }
+}
